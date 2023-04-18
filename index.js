0186b17d03d661eb05d93377b2971f5c273ecdef
Beka Chaduneli <120742353+bekaChaduneli@users.noreply.github.com>
2023-04-18 20:46:20 +0400

    Format index.js

--- index.js
+++ index.js
@@ -1,24 +1,22 @@
 "use strict";
 var faq = document.querySelectorAll(".one-line");
-var arr = document.querySelectorAll('.question__button');
+var arr = document.querySelectorAll(".question__button");
 var i;
 
 for (i = 0; i < faq.length; i++) {
-    faq[i].addEventListener("click", function(event) {
-      var panel = this.nextElementSibling;
-      if (panel.style.display === "block") {
-        panel.style.display = "none";
-        this.querySelector("#uploadPreview").classList.remove('active');
-        this.querySelector("#question").classList.remove('after');
-
-      } 
-      else {
-        for (i = 0; i < faq.length; i++) {
-          faq[i].nextElementSibling.style.display = "none";
-        }
-        panel.style.display = "block";
-        this.querySelector("#uploadPreview").classList.add('active');
-        this.querySelector("#question").classList.add('after');
+  faq[i].addEventListener("click", function (event) {
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+      this.querySelector("#uploadPreview").classList.remove("active");
+      this.querySelector("#question").classList.remove("after");
+    } else {
+      for (i = 0; i < faq.length; i++) {
+        faq[i].nextElementSibling.style.display = "none";
       }
-    });
-  }
+      panel.style.display = "block";
+      this.querySelector("#uploadPreview").classList.add("active");
+      this.querySelector("#question").classList.add("after");
+    }
+  });
+}
